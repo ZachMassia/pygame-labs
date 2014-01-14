44a5fbeda325f61591276eedfab25529aab3a419
Zachary Massia <zmassia@gmail.com>
2014-01-14 01:58:57 -0500

    Move the mandatory methods to the top of the class.

--- lab2/part1.py
+++ lab2/part1.py
@@ -13,6 +13,34 @@ class Game(object):
         self.key_mario_pos = pygame.math.Vector2()
         self.mouse_mario_pos = pygame.math.Vector2()
 
+    def update(self, dt):
+        """Update the game logic."""
+        self.key_mario_pos += self.input_vec * dt
+        self.mouse_mario_pos = pygame.mouse.get_pos()
+
+        self.input_vec = pygame.math.Vector2()  # reset input vec.
+
+    def draw(self):
+        """Blit surfaces to the display surface."""
+        # Keyboard Mario
+        self.SCR_SURF.blit(self.mario_img, (self.key_mario_pos.x,
+                                            self.key_mario_pos.y))
+        # Mouse Mario
+        self.SCR_SURF.blit(self.mario_img, self.mouse_mario_pos)
+
+    def build(self):
+        """Called before the game loop starts."""
+        # Load mario at scale him
+        self.mario_img = pygame.image.load("mario.png")
+        self.mario_img = pygame.transform.scale(self.mario_img, (64, 64))
+        # Use (0,0) as colorkey
+        self.mario_img.set_colorkey(self.mario_img.get_at((0, 0)))
+
+        self.setup_event_handlers()
+
+        # Use key repeat
+        pygame.key.set_repeat(25, 25)
+
     def move_key_mario(self, evt):
         """Update the keyboard input vector on key down."""
         # X Axis
@@ -47,34 +75,6 @@ class Game(object):
         elif evt.key == pygame.K_4:
             num = 4
         #TODO Actually play the sound
-
-    def update(self, dt):
-        """Update the game logic."""
-        self.key_mario_pos += self.input_vec * dt
-        self.mouse_mario_pos = pygame.mouse.get_pos()
-
-        self.input_vec = pygame.math.Vector2()  # reset input vec.
-
-    def draw(self):
-        """Blit surfaces to the display surface."""
-        # Keyboard Mario
-        self.SCR_SURF.blit(self.mario_img, (self.key_mario_pos.x,
-                                            self.key_mario_pos.y))
-        # Mouse Mario
-        self.SCR_SURF.blit(self.mario_img, self.mouse_mario_pos)
-
-    def build(self):
-        """Called before the game loop starts."""
-        # Load mario at scale him
-        self.mario_img = pygame.image.load("mario.png")
-        self.mario_img = pygame.transform.scale(self.mario_img, (64, 64))
-        # Use (0,0) as colorkey
-        self.mario_img.set_colorkey(self.mario_img.get_at((0, 0)))
-
-        self.setup_event_handlers()
-
-        # Use key repeat
-        pygame.key.set_repeat(25, 25)
 
     def setup_event_handlers(self):
         self.evt_mgr.subscribe(pygame.KEYDOWN, self.move_key_mario)
