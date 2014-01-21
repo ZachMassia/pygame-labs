d9bb0be6d7c81c806ee033335ca9cd6a6ed83ddd
Zachary Massia <zmassia@gmail.com>
2014-01-21 15:47:26 -0500

    Add finish line sprite

--- lab3/part1.py
+++ lab3/part1.py
@@ -6,26 +6,45 @@ from basic_game.app import App
 from car import Car
 
 
+class FinishLine(pygame.sprite.Sprite):
+    """Basic white line sprite."""
+
+    def __init__(self, screen_surf, width, percent_towards_right):
+        # Parent class constructor must be called.
+        super(FinishLine, self).__init__()
+
+        scr_size = screen_surf.get_size()
+
+        # Create a surface and fill it white.
+        self.image = pygame.Surface((scr_size[0] * width, scr_size[1]))
+        self.image.fill((255, 255, 255))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = scr_size[0] * percent_towards_right
+
+
 class Game(object):
+    """Main game class. Contains all game logic."""
 
     def __init__(self):
         self.evt_mgr = None   # Injected by App
         self.scr_surf = None  # Injected by App
 
         self.sounds = list()
-        self.cars = pygame.sprite.Group()
+        self.sprites = pygame.sprite.Group()
 
     def update(self, dt):
         """Update the game logic."""
-        self.cars.update(dt)
+        self.sprites.update(dt)
 
     def draw(self):
         """Blit surfaces to the display surface."""
-        self.cars.draw(self.scr_surf)
+        self.sprites.draw(self.scr_surf)
 
     def build(self):
         """Called before the game loop starts and after pygame is initialized."""
         self.setup_cars()
+        self.setup_finish_line()
         self.setup_event_handlers()
 
         # Use key repeat
@@ -47,7 +66,11 @@ class Game(object):
         blue_car.vel.x = x_vel * 0.9
 
         # Register the cars with the Sprite group.
-        self.cars.add(red_car, blue_car)
+        self.sprites.add(red_car, blue_car)
+
+    def setup_finish_line(self):
+        """Create the finish line surface."""
+        self.sprites.add(FinishLine(self.scr_surf, 0.03, 0.8))
 
     def setup_event_handlers(self):
         """Register methods with specific Pygame events."""
