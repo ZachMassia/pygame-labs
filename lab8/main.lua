0795b7d26db40c65ca263153c6ec4b9e701573e2
Zachary Massia <zmassia@gmail.com>
2014-04-16 20:19:39 -0400

    Finish lab8

--- lab8/main.lua
+++ lab8/main.lua
@@ -1,30 +1,108 @@
 require('iuplua')
 
+-- Cursor --
+img_cursor = iup.image{
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,1,2,2,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,1,1,2,0,0,2,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 }
+  -- Sets cursor image colors
+  ; colors = { "255 0 0", "128 0 0" }, hotspot = "21:10"
+}
+
 -- Text Box --
 textBox = iup.multiline {expand='YES'}
-
-
-----------------------------------------------------------------------
 
 -- Buttons -- 
 local textColourBtn = iup.button {title='Text'}
 local bgColourBtn = iup.button {title='Background'}
 
 function textColourBtn:action()
-  
+  local dlg = iup.colordlg{}
+  iup.Popup(dlg, iup.CENTER, iup.CENTER)
+  textBox.fgcolor = dlg.value
 end
 
 function bgColourBtn:action()
-
+  local dlg = iup.colordlg{}
+  iup.Popup(dlg, iup.CENTER, iup.CENTER)
+  textBox.bgcolor = dlg.value
 end
 
-btnBox = iup.hbox {textColourBtn, bgColourBtn; gap=4, expand='NO'}
-----------------------------------------------------------------------
+btnBox = iup.hbox {iup.fill{}, textColourBtn, bgColourBtn; gap=4, expand='YES', 
+                   iup.fill{}}
 
+-- Menu --
+
+-- Create the Open item
+open_item = iup.item {title='Open'}
+function open_item:action()
+  local dlg = iup.filedlg{}
+  iup.Popup(dlg, iup.CENTER, iup.CENTER)
+  
+  -- Make sure user didn't cancel (-1)
+  if dlg.status ~= -1 then
+    for line in io.lines(dlg.value) do
+      textBox.append = line
+    end
+  end
+end
+
+-- Create the Exit item
+exit_item = iup.item {title='Exit'}
+function exit_item:action()
+  iup.ExitLoop()
+end
+
+-- Create the file menu
+file_menu = iup.menu {open_item, exit_item}
+file_submenu = iup.submenu {file_menu; title='File'}
+
+-- Create the About item
+about_item = iup.item {title='About'}
+function about_item:action()
+  iup.Message('About this App', 
+              'Scripting Class - Lab 8\nWritten by Zachary Massia')
+end
+
+help_submenu = iup.submenu {iup.menu{about_item}; title='Help'}
+
+menu = iup.menu {file_submenu, help_submenu}
+
+-- App --
 appBox = iup.vbox {textBox, btnBox; aligment='ACENTER'}
 
-dialog = iup.dialog {appBox; title='Lua Scripting Lab 8', size='HALFxHALF'}
+dialog = iup.dialog {appBox; title='Lua Scripting Lab 8', size='HALFxHALF',
+                     menu=menu, cursor=img_cursor}
 
 -- Kick off the application
-dialog:show()
+dialog:show(iup.CENTER, iup.CENTER)
 iup.MainLoop()
